0a3df6ab722f9f104a5d8677c0a270a7c50332c0
eyoon <euiseonyoon@gmail.com>
2025-07-04 16:29:16 +0900

    chore: add comment

--- src/main/kotlin/com/example/JPA/study/springdata/common/repositories/customgeneral/MyRepository.kt
+++ src/main/kotlin/com/example/JPA/study/springdata/common/repositories/customgeneral/MyRepository.kt
@@ -5,7 +5,8 @@ import org.springframework.data.repository.NoRepositoryBean
 import java.io.Serializable
 
 // 모든 entity에 적용될 공통
-// NOTE: 이거 중요!!
+// NOTE: 이거 중요!!  다른 Repository에서 사용될 중간 Repository에는 @NoRepositoryBean를 꼭 넣는다
+// e.g.). AccountRepository - MyRepository(중간) - JpaRepository
 @NoRepositoryBean
 interface MyRepository<T, ID : Serializable>: JpaRepository<T, ID> {
 
