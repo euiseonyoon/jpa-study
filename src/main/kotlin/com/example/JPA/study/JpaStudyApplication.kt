53a17cecacd57d659ac7436e5a76e188c8bd4f2d
eyoon <euiseonyoon@gmail.com>
2025-07-04 10:52:41 +0900

    init

---
+++ src/main/kotlin/com/example/JPA/study/JpaStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.JPA.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<JpaStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/JPA/study/JpaStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.JPA.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
