66b2af1df3ffa7e13b59289beaf275101afd1882
eyoon <euiseonyoon@gmail.com>
2025-07-04 16:18:02 +0900

    feat: QueryDsl3

--- src/main/kotlin/com/example/JPA/study/springdata/common/querydsl/AccountRepository.kt
+++ src/main/kotlin/com/example/JPA/study/springdata/common/querydsl/AccountRepository.kt
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.querydsl.QuerydslPredicateExecutor
 
 // reactive webflux 일때는 ReactiveQuerydslPredicateExecutor를 사용하면 되겠다.
-interface AccountRepository: JpaRepository<Account, Long>, QuerydslPredicateExecutor<Account> {
+interface AccountRepository: JpaRepository<Account, Long>, QuerydslPredicateExecutor<Account>, AccountExtension {
 }
