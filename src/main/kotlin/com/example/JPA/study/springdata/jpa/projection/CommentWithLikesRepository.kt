1c57df13c022e1987106e69e715fbb3490dbac72
eyoon <euiseonyoon@gmail.com>
2025-07-05 19:05:45 +0900

    feat: Projection

---
+++ src/main/kotlin/com/example/JPA/study/springdata/jpa/projection/CommentWithLikesRepository.kt
@@ -0,0 +1,18 @@
+package com.example.JPA.study.springdata.jpa.projection
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+interface CommentWithLikesRepository: JpaRepository<CommentWithLikes, Long> {
+
+    fun findByPost_Id(id: Long): List<CommentWithLikes>
+
+    @Query("SELECT c FROM CommentWithLikes c WHERE c.post.id = :id")
+    fun findWithPost(@Param("id") id: Long): List<CommentProjection>
+
+
+    @Query("SELECT new com.example.JPA.study.springdata.jpa.projection.CommentDto(c.likes, c.dislikes) FROM CommentWithLikes c WHERE c.post.id = :id")
+    fun findCommentDtoById(@Param("id") id: Long): List<CommentDto>
+
+}
