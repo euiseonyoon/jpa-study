ef38675f3f2ee353340152361e56bd2f55f81a5d
eyoon <euiseonyoon@gmail.com>
2025-07-05 19:05:45 +0900

    feat: Projection

---
+++ src/main/kotlin/com/example/JPA/study/springdata/jpa/projection/CommentWithLikes.kt
@@ -0,0 +1,18 @@
+package com.example.JPA.study.springdata.jpa.projection
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+
+@Entity
+class CommentWithLikes(
+    @Id @GeneratedValue
+    val id: Long? = null,
+    val content: String,
+) {
+    var likes: Int = 0
+    var dislikes: Int = 0
+    @ManyToOne()
+    var post: PostWithLikeComments? = null
+}
