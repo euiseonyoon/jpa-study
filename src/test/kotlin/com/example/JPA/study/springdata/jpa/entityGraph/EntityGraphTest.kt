a92734c402d51b3fed7c0886b6d1a73cde6afcb2
eyoon <euiseonyoon@gmail.com>
2025-07-06 19:42:11 +0900

    feat: EntityGraph 관련 수정
    - Store.addProduct()를 통해 생성된 product들을 store에 매핑해줬어야 한다..., 조심하자

--- src/test/kotlin/com/example/JPA/study/springdata/jpa/entityGraph/EntityGraphTest.kt
+++ src/test/kotlin/com/example/JPA/study/springdata/jpa/entityGraph/EntityGraphTest.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.PersistenceContext
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import kotlin.test.assertTrue
 
 @DataJpaTest
 class EntityGraphTest {
@@ -26,11 +27,12 @@ class EntityGraphTest {
         }
 
         val savedProducts = (0..19).map { it ->
-            val product = if (it <= 9) {
-                Product(name = "$it product", price = it * 100, store = savedStore1)
+            val (product, store) = if (it <= 9) {
+                Product(name = "$it product", price = it * 100, store = savedStore1) to savedStore1
             } else {
-                Product(name = "$it product", price = it * 120, store = savedStore2)
+                Product(name = "$it product", price = it * 120, store = savedStore2) to savedStore2
             }
+            store.addProduct(product)
             productRepository.save(product)
         }
         em.flush()
@@ -40,7 +42,16 @@ class EntityGraphTest {
     @Test
     fun test() {
         addStoreAndProduct()
-        em.clear()
+
+        val allStores = storeRepository.findAll()
+        allStores.map { store ->
+            assertTrue { store.productList.isNotEmpty() }
+
+            store.productList.map { product ->
+                assertTrue { product.store.id == store.id }
+                assertTrue { product.store.productList.contains(product) }
+            }
+        }
 
         // 1 + N
         val foundStores = storeRepository.findAll()
