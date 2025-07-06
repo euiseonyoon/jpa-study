e0b773329f83f5e48968a481a08b2206c273cbdb
eyoon <euiseonyoon@gmail.com>
2025-07-06 20:35:19 +0900

    feat: Specification

---
+++ src/main/kotlin/com/example/JPA/study/springdata/jpa/specification/Product2Repository.kt
@@ -0,0 +1,6 @@
+package com.example.JPA.study.springdata.jpa.specification
+
+import com.example.JPA.study.springdata.common.repositories.customgeneral.MyRepository
+
+interface Product2Repository : MyRepository<Product2, Long> {
+}
---
+++ src/main/kotlin/com/example/JPA/study/springdata/jpa/specification/Store2.kt
@@ -0,0 +1,31 @@
+package com.example.JPA.study.springdata.jpa.specification
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.NamedAttributeNode
+import jakarta.persistence.NamedEntityGraph
+import jakarta.persistence.OneToMany
+
+@Entity
+@NamedEntityGraph(
+    name = "Store2.withProducts",
+    attributeNodes = [
+        NamedAttributeNode("products")
+    ]
+)
+class Store2(
+    @Id @GeneratedValue
+    val id: Long? = null,
+    val name : String,
+) {
+    @OneToMany(mappedBy = "store")
+    private val products: MutableSet<Product2> = mutableSetOf()
+
+    val productList: List<Product2>
+        get() = products.toList()
+
+    fun addProduct(product: Product2) {
+        this.products.add(product)
+    }
+}
