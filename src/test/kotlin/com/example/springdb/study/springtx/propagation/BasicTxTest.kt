6243c9ea8fce3322c57fe140ce821c7e3d86dc1a
eyoon <euiseonyoon@gmail.com>
2025-07-13 19:07:43 +0900

    feat: @Transactional propagation - 2 (Basic)

--- src/test/kotlin/com/example/springdb/study/springtx/propagation/BasicTxTest.kt
+++ src/test/kotlin/com/example/springdb/study/springtx/propagation/BasicTxTest.kt
@@ -79,4 +79,80 @@ class BasicTxTest {
          *
          * */
     }
+
+    @Test
+    fun doubleCommit() {
+        log.info("tx1 시작")
+        val tx1: TransactionStatus = txManager.getTransaction(DefaultTransactionAttribute())
+        log.info("tx1 커밋 시작")
+        txManager.commit(tx1)
+        log.info("tx1 커밋 완료")
+
+        log.info("tx2 시작")
+        val tx2: TransactionStatus = txManager.getTransaction(DefaultTransactionAttribute())
+        log.info("tx2 커밋 시작")
+        txManager.commit(tx2)
+        log.info("tx2 커밋 완료")
+
+        /**
+         * LOG:
+         *
+         * 아래 로그를 보면 각각의 TX 수행시 다른 db 커넥션을 사용했음을 볼 수 있다.
+         *     tx1: HikariProxyConnection@1978823220
+         *     tx2: HikariProxyConnection@1680799504
+         *
+         * tx1 시작
+         * Creating new transaction with name [null]: PROPAGATION_REQUIRED,ISOLATION_DEFAULT
+         * Acquired Connection [HikariProxyConnection@1978823220 wrapping org.postgresql.jdbc.PgConnection@430212cb] for JDBC transaction
+         * tx1 커밋 시작
+         * Initiating transaction commit
+         * Committing JDBC transaction on Connection [HikariProxyConnection@1978823220 wrapping org.postgresql.jdbc.PgConnection@430212cb]
+         * Releasing JDBC Connection [HikariProxyConnection@1978823220 wrapping org.postgresql.jdbc.PgConnection@430212cb] after transaction
+         * tx1 커밋 완료
+         *
+         * tx2 시작
+         * Creating new transaction with name [null]: PROPAGATION_REQUIRED,ISOLATION_DEFAULT
+         * Acquired Connection [HikariProxyConnection@1680799504 wrapping org.postgresql.jdbc.PgConnection@430212cb] for JDBC transaction
+         * tx2 커밋 시작
+         * Initiating transaction commit
+         * Committing JDBC transaction on Connection [HikariProxyConnection@1680799504 wrapping org.postgresql.jdbc.PgConnection@430212cb]
+         * Releasing JDBC Connection [HikariProxyConnection@1680799504 wrapping org.postgresql.jdbc.PgConnection@430212cb] after transaction
+         * tx2 커밋 완료
+         *
+         * */
+    }
+
+    @Test
+    fun commitAndRollback() {
+        log.info("tx1 시작")
+        val tx1: TransactionStatus = txManager.getTransaction(DefaultTransactionAttribute())
+        log.info("tx1 커밋 시작")
+        txManager.commit(tx1)
+
+        log.info("tx2 시작")
+        val tx2: TransactionStatus = txManager.getTransaction(DefaultTransactionAttribute())
+        log.info("tx2 롤백 시작")
+        txManager.rollback(tx2)
+
+        /**
+         * LOG:
+         *
+         * tx1 시작
+         * Creating new transaction with name [null]: PROPAGATION_REQUIRED,ISOLATION_DEFAULT
+         * Acquired Connection [HikariProxyConnection@1251888103 wrapping org.postgresql.jdbc.PgConnection@67a3dd86] for JDBC transaction
+         * tx1 커밋 시작
+         * Initiating transaction commit
+         * Committing JDBC transaction on Connection [HikariProxyConnection@1251888103 wrapping org.postgresql.jdbc.PgConnection@67a3dd86]
+         * Releasing JDBC Connection [HikariProxyConnection@1251888103 wrapping org.postgresql.jdbc.PgConnection@67a3dd86] after transaction
+         *
+         * tx2 시작
+         * Creating new transaction with name [null]: PROPAGATION_REQUIRED,ISOLATION_DEFAULT
+         * Acquired Connection [HikariProxyConnection@2109521019 wrapping org.postgresql.jdbc.PgConnection@67a3dd86] for JDBC transaction
+         * tx2 롤백 시작
+         * Initiating transaction rollback
+         * Rolling back JDBC transaction on Connection [HikariProxyConnection@2109521019 wrapping org.postgresql.jdbc.PgConnection@67a3dd86]
+         * Releasing JDBC Connection [HikariProxyConnection@2109521019 wrapping org.postgresql.jdbc.PgConnection@67a3dd86] after transaction
+         *
+         * */
+    }
 }
